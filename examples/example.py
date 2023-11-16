7cd9c3958e20484baad3569d81f35cb210870a1c
Andrew French <andrew@afren.ch>
2023-11-16 14:52:35 -0800

    Reformat

--- examples/example.py
+++ examples/example.py
@@ -4,14 +4,10 @@ from griptape.structures import Agent
 from proxycurl_client import ProxycurlClient
 
 # Create the ProxycurlClient tool
-proxycurl_tool = ProxycurlClient(
-    proxycurl_api_key=os.environ["PROXYCURL_API_KEY"]
-)
+proxycurl_tool = ProxycurlClient(proxycurl_api_key=os.environ["PROXYCURL_API_KEY"])
 
 # Set up an agent using the ProxycurlClient tool
-agent = Agent(
-    tools=[proxycurl_tool, TaskMemoryClient(off_prompt=False)]
-)
+agent = Agent(tools=[proxycurl_tool, TaskMemoryClient(off_prompt=False)])
 
 # Task: Fetch information for the company Griptape.ai
 agent.run("Tell me about the company profile Griptape.ai.")
